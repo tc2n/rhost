e775c271ded7bb524e977a781d18e4304f90e77b
tusharc2n <tusharc2n@gmail.com>
2021-11-01 16:51:52 +0530

    Changes made to accept backend data

--- src/components/FileItem.vue
+++ src/components/FileItem.vue
@@ -1,26 +1,52 @@
 <template>
-    <li :key="item.id" v-for="item in file_items" class="file" @click="onClick(item.id)" :class="[active==item.id ? 'file--active': '']">
-        <img :src="item.directory?folder_thumbnail:file_thumbnail" alt="icon" class="file__icon">
-        <div class="file__name">{{item.name}}</div>
-    </li>
+  <li
+    :key="index"
+    v-for="(item, index) in file_items"
+    class="file"
+    @click="onClick(index)"
+    @dblclick="openFile(index)"
+    :class="[active == index ? 'file--active' : '']"
+  >
+    <img
+      :src="isDirectory(item) ? folder_thumbnail : file_thumbnail"
+      alt="icon"
+      class="file__icon"
+    />
+    <div class="file__name">{{ item[2] }}</div>
+  </li>
 </template>
 
 // ../assets/img/unknown_file_thumbnail.svg
 <script>
 export default {
-    name:'FileItem',
-    props: {
-        file_items:Array
+  name: "FileItem",
+  props: {
+    file_items: Array,
+  },
+  methods: {
+    onClick(id) {
+      this.active = id;
+      this.$emit("selected-id", id);
     },
-    methods:{
-        onClick(id){
-            this.active=id
-            this.$emit('selected-id', id)
+    openFile(id) {
+        this.$emit("open-file", id)
+    },
+    isDirectory(item){
+        if(item[0][0]==='d'){
+            return true;
         }
+        return false;
     },
-    data(){
-        return {active:-1, folder_thumbnail: require('../assets/img/folder_thumbnail.svg'), file_thumbnail: require('../assets/img/unknown_file_thumbnail.svg')}
-    },
-    emits: ['selected-id']
-}
+    
+  },
+  data() {
+    return {
+      active: -1,
+      folder_thumbnail: require("../assets/img/folder_thumbnail.svg"),
+      file_thumbnail: require("../assets/img/unknown_file_thumbnail.svg"),
+    };
+  },
+  emits: ["selected-id", "open-file"],
+  
+};
 </script>
